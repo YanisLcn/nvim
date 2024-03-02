06351902d1949ca749518c0efe83dd2597987364
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-03-02 02:28:59 +0100

    feat(plugins): Install plugins

--- lua/lore/lazy.lua
+++ lua/lore/lazy.lua
@@ -3,46 +3,79 @@ vim.g.localmapleader = ' '
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system {
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github/folke/lazy.nvim.git',
-		'--branch=stable',
-		lazypath,
-	}
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-	-- [[ Colorschemes ]]
-	
-	-- https://github.com/ellisonleao/gruvbox.nvim
-	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+  -- [[ Colorschemes ]]
 
+  -- https://github.com/ellisonleao/gruvbox.nvim
+  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
 
-	-- [[ Treesitter ]]
-	
-	-- https://github.com/nvim-treesitter/nvim-treesitter
-	{   "nvim-treesitter/nvim-treesitter",
-	    build = ":TSUpdate",
-	    config = function () 
-	      local configs = require("nvim-treesitter.configs")
+  -- [[ Treesitter ]]
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+  },
 
-	      configs.setup({
-		  ensure_installed = { "c", "lua", "vim", "vimdoc", "html" },
-		  sync_install = false,
-		  highlight = { enable = true },
-		  indent = { enable = true },  
-		})
-	    end
-	},
+  -- [[ Telescope ]]
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+  },
 
-	-- [[ Telescope ]]
-	
-	-- https://github.com/nvim-telescope/telescope.nvim
-	{
-	    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	      dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' }
-	},
+  -- [[ Autocompletion ]]
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/nvim-cmp',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+
+  -- [[ Mason - LSP - DAP - Formatters ]]
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      { 'folke/neodev.nvim', opts = {} },
+    },
+  },
+
+  { 'mhartington/formatter.nvim' },
+
+  -- [[ Git ]]
+  {
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+  },
 }, {})
