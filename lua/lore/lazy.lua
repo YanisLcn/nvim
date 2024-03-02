147bd780583f2ad536a7a82d1e94c47a8a4cca7c
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-03-03 00:46:34 +0100

    feat(plugin): Install gitsigns and which-key

--- lua/lore/lazy.lua
+++ lua/lore/lazy.lua
@@ -3,93 +3,102 @@ vim.g.localmapleader = ' '
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- [[ Colorschemes ]]
+    -- [[ Colorschemes ]]
 
-  -- https://github.com/ellisonleao/gruvbox.nvim
-  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
+    -- https://github.com/ellisonleao/gruvbox.nvim
+    { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
 
-  -- [[ Treesitter ]]
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-  },
+    -- [[ Treesitter ]]
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+    },
 
-  -- [[ Telescope ]]
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
+    -- [[ Telescope ]]
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                -- NOTE: If you are having trouble with this installation,
+                --       refer to the README for telescope-fzf-native for more instructions.
+                build = 'make',
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end,
+            },
+        },
+    },
+
+    -- [[ Autocompletion ]]
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+
+            -- Adds LSP completion capabilities
+            'hrsh7th/cmp-nvim-lsp',
+            'neovim/nvim-lspconfig',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/nvim-cmp',
+
+            -- Adds a number of user-friendly snippets
+            'rafamadriz/friendly-snippets',
+        },
+    },
+
+    -- [[ Mason - LSP - DAP - Formatters ]]
+    {
+        -- LSP Configuration & Plugins
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            -- Automatically install LSPs to stdpath for neovim
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+
+            -- Useful status updates for LSP
+            { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+
+            -- Additional lua configuration, makes nvim stuff amazing!
+            { 'folke/neodev.nvim', opts = {} },
+        },
+    },
+
+    { 'mhartington/formatter.nvim' },
+
+    -- [[ Git ]]
+    {
+        'lewis6991/gitsigns.nvim',
+    },
+
+    -- [[ Miscellaneous ]]
+    { 'numToStr/Comment.nvim', opts = {} },
+
+    {
+        'folke/which-key.nvim',
+        event = 'VeryLazy',
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
         end,
-      },
     },
-  },
-
-  -- [[ Autocompletion ]]
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'neovim/nvim-lspconfig',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/nvim-cmp',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
-  },
-
-  -- [[ Mason - LSP - DAP - Formatters ]]
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      { 'folke/neodev.nvim', opts = {} },
-    },
-  },
-
-  { 'mhartington/formatter.nvim' },
-
-  -- [[ Git ]]
-  {
-    'lewis6991/gitsigns.nvim',
-  },
-
-  -- [[ Miscellaneous ]]
-  { 'numToStr/Comment.nvim', opts = {} },
 }, {})
