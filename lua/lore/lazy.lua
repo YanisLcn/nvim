2f2672c09284d65e15ae36061f58a3ef2ad6fbc0
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-03-03 01:59:26 +0100

    feat(plugin): Install lualine

--- lua/lore/lazy.lua
+++ lua/lore/lazy.lua
@@ -100,4 +100,16 @@ require('lazy').setup({
             vim.o.timeoutlen = 300
         end,
     },
+    {
+        -- Set lualine as statusline
+        'nvim-lualine/lualine.nvim',
+        opts = {
+            options = {
+                icons_enabled = true,
+                theme = 'onedark',
+                component_separators = '|',
+                section_separators = '',
+            },
+        },
+    },
 }, {})
