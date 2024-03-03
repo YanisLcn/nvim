1ff0ba6cf51668a93c7fa26ba0c98214056b6019
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-03-03 01:39:28 +0100

    style(colorscheme): Change NonText's color

--- lua/lore/plugins/colorscheme.lua
+++ lua/lore/plugins/colorscheme.lua
@@ -1,3 +1,8 @@
 -- Gruvbox.nvim
-vim.o.background = "dark"
-vim.cmd([[colorscheme gruvbox]])
+vim.o.background = 'dark'
+
+require('gruvbox').setup {
+    overrides = { SignColumn = { bg = '#282828' }, NonText = { fg = '#928374' } },
+}
+
+vim.cmd [[colorscheme gruvbox]]
